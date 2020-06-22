eb8df41049dede2a1e902dd8968b16e392ca26ef
Luciano Nery <lsnery@gmail.com>
2020-06-21 23:15:01 -0300

    style: formmat

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,6 +14,7 @@ describe('SignUp Controller', () => {
         };
 
         const httpResponse = sut.handle({ httpRequest });
+
         expect(httpResponse.statusCode).toBe(400);
         expect(httpResponse.body).toEqual(new MissingParamError('name'));
     });
@@ -30,6 +31,7 @@ describe('SignUp Controller', () => {
         };
 
         const httpResponse = sut.handle({ httpRequest: request });
+
         expect(httpResponse.statusCode).toBe(400);
         expect(httpResponse.body).toEqual(new MissingParamError('email'));
     });
@@ -46,6 +48,7 @@ describe('SignUp Controller', () => {
         };
 
         const httpResponse = sut.handle({ httpRequest: request });
+
         expect(httpResponse.statusCode).toBe(400);
         expect(httpResponse.body).toEqual(new MissingParamError('password'));
     });
@@ -62,6 +65,7 @@ describe('SignUp Controller', () => {
         };
 
         const httpResponse = sut.handle({ httpRequest: request });
+
         expect(httpResponse.statusCode).toBe(400);
         expect(httpResponse.body).toEqual(
             new MissingParamError('passwordConfirmation')
